c09a34bf495a9b4a767e3d05985e7339db4d6b5f
Webb Roberts <webb.roberts@gtri.gatech.edu>
2014-02-27 09:19:27 -0500

    Added COMMAND_REL, to give relative path to the current script. Removed opt_help. Use lib/fn_opt_help.bash instead.

--- lib/common.bash
+++ lib/common.bash
@@ -2,12 +2,9 @@
 if test is-set != "${LOADED_COMMON_BASH:+is-set}"
 then LOADED_COMMON_BASH=true
 
-    COMMAND_NAME=$(basename "$0")
-    COMMAND_ABS=$(cd "$(dirname "$0")"; pwd)/$COMMAND_NAME
+    COMMAND_REL=$0
+    COMMAND_NAME=$(basename "$COMMAND_REL")
+    COMMAND_ABS=$(cd "$(dirname "$COMMAND_REL")"; pwd)/$COMMAND_NAME
 
-    opt_help () {
-        printf '%s' "$COMMAND_NAME"
-        sed -e "s/.*#""HELP://p;d" "$COMMAND_ABS"
-    }
 fi
 
